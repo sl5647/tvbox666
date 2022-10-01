4960829c2ea24ff903b327f3be8cea6bfcd07810
sl5647 <381203888@qq.com>
2022-10-01 23:36:57 +0800

    更新 ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -81,7 +81,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://52bsj.vip:81/api/v3/file/get/52654/TVBox.json?sign=P49kdDzg6kUSp8shbu-AtL_YJOhz21c0CeExrquysiQ%3D%3A0");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
