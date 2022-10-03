01f43cb366595b859ee9c626fdc376cc2d5ec7eb
sl5647 <381203888@qq.com>
2022-10-04 00:29:37 +0800

    修改播放模式

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -56,7 +56,7 @@ public class App extends MultiDexApplication {
         Hawk.init(this).build();
         Hawk.put(HawkConfig.DEBUG_OPEN, false);
         if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
-            Hawk.put(HawkConfig.PLAY_TYPE, 2);
+            Hawk.put(HawkConfig.PLAY_TYPE, 1);
         }
         //硬解码
         if (!Hawk.contains(HawkConfig.IJK_CODEC)) {
